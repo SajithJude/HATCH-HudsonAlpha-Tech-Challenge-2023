d842fcf35752eddac31ab25ea4a135ce684993ea
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-05 20:55:34 +0000

    sss

--- app.py
+++ app.py
@@ -4,75 +4,18 @@ import openai
 import os 
 
 
-openai.api_key =  os.getenv("APIKEY")
-
-
-with st.expander("How to use"):
-            st.markdown("""
+st.markdown("# 4-1 ai chatbot gpt 3 based IT website")
+st.markdown("""
     
-                #### Upload your sequencing dataset in CSV format, 
-                #### Type a one line description about the dataset,
-                #### Select all the columns in the dataset
-                #### Enter a one line description about the Coulumn in each input field and press enter
-                #### Click Generate Explanation to understand the best practice and reasons to select columns
-                #### Click Generate code to Generate a python Machine learning code to train the model with the selected parameters
-                """)
+#### Click on the arrow icon on the top left to trigger menu
+#### Error Check :
+    # Allows you to Paste your code and input which programming language that was used
+    # If Errors Found in code, It will fix the error, if not, It will modify the code to make it efficient 
+#### CS Dissertation
+    # Allows you to Paste Reference URLs of Publications(around 4 atm) 
+    # Will analyze the entire publication and generate a paragraph for Literature review, Novelity, Citations, ect..
+#### Knowledge From Books:
+    # Allows you to paste URL of any books
+    # Will Reply to any questions, where the answers will be based on the information on the book 
 
-# Read CSV file
-uploaded_f = st.file_uploader('Upload CSV', type=['csv'])
-
-if uploaded_f is not None:
-# Display column selection checkboxes
-    df = pd.read_csv(uploaded_f)
-    selected_columns = st.multiselect('Select columns', df.columns)
-
-    # Create dictionary of selected column descriptions
-    column_descriptions = {}
-    context= st.text_input("Enter the context of the data :")
-    for col in selected_columns:
-        description = st.text_input(f'{col} Describe the data in relation to the context')
-        column_descriptions[col] = description
-
-    # Store selected columns as variables with their respective descriptions
-    for col, description in column_descriptions.items():
-        exec(f"{col} = '{description}'")
-
-    # Display selected column names and descriptions
-    for col, description in column_descriptions.items():
-        st.write(f"{col}: {description}")
-
-if st.button("Generate Explanation :"):
-
-    inpt = "Generate an explanation on which of the following columns to as training parameters for a dataset about " + context + " . " + "Column description :" +  str(column_descriptions)
-    st.write(inpt)
-
-    outpt = openai.Completion.create(
-                                        engine="text-davinci-003",
-                                        prompt=inpt,
-                                        max_tokens=3600,
-                                        n=1,
-                                        stop=None,
-                                        temperature=0.5,
-                                        )
-    explan= outpt.choices[0].text.strip()
-    st.write(explan)
-    st.stop()
-
-if st.button("Generate Code :"):
-
-    inpt_code = "Generate training code in python selecting the best parameters from the following columns of a dataset about " + context + " . " + "Column description :" +  str(column_descriptions)
-    st.write(inpt_code)
-
-    outpt = openai.Completion.create(
-                                        engine="text-davinci-003",
-                                        prompt=inpt_code,
-                                        max_tokens=3600,
-                                        n=1,
-                                        stop=None,
-                                        temperature=0.5,
-                                        )
-    explan_code= outpt.choices[0].text.strip()
-    st.code(explan_code)
-    st.stop()
-
-
+""")
