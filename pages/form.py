0b0436b3175987d048d2d7f98ae344fb73fb9f01
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 07:56:01 +0000

    sss

--- pages/form.py
+++ pages/form.py
@@ -1,60 +1,61 @@
 import streamlit as st
 from streamlit.components.v1 import html
 
-# Define the HTML template for the glassmorphism card
-html_template = """
-<div style="background: rgba(255, 255, 255, 0.5);
-            backdrop-filter: blur(5px);
-            border-radius: 10px;
-            box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-            color: #000;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 300px;
-            width: 500px;
-            margin: auto;">
-    <form>
-        <label for="input-text" style="font-size: 24px;">Enter Text:</label>
-        <br>
-        <input type="text" id="input-text" name="input-text" style="width: 80%;
-            padding: 12px 20px;
-            margin: 8px 0;
-            box-sizing: border-box;
-            border: 2px solid #ccc;
-            border-radius: 4px;
-            background-color: #f8f8f8;
-            font-size: 16px;">
-        <br>
-        <button type="submit" style="background-color: #4CAF50;
-            border: none;
-            color: white;
-            padding: 16px 32px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            cursor: pointer;
-            border-radius: 4px;">Submit</button>
-    </form>
+# Define the HTML for the top navigation menu
+top_menu = """
+<div style="background: linear-gradient(to right, #9C27B0, #E040FB); padding: 10px;">
+    <span style="color: white; font-size: 30px;">Glassmorphism Page</span>
 </div>
 """
 
-# Define the callback function for when the form is submitted
-def on_submit(input_text):
-    st.write(f"You entered: {input_text}")
+# Define the HTML for the sidebar
+sidebar = """
+<div style="background: linear-gradient(to bottom, #9C27B0, #E040FB); padding: 10px;">
+    <span style="color: white; font-size: 20px;">Sidebar</span>
+</div>
+"""
 
-# Render the glassmorphism card with the text input field
-html_result = html_template.format()
-form = html(html_result)
+# Define the HTML for the glassmorphism card
+card = """
+<div style="background: linear-gradient(to bottom right, #9C27B0, #E040FB); border-radius: 20px; padding: 20px;">
+    <div style="display: flex; flex-direction: row; justify-content: space-between; margin-bottom: 20px;">
+        <div style="background: linear-gradient(to bottom, #FF6B6B, #FFC107); border-radius: 20px; padding: 20px; margin-right: 10px;">
+            <span style="color: white; font-size: 20px;">Column 1</span>
+        </div>
+        <div style="background: linear-gradient(to bottom, #64B5F6, #00E676); border-radius: 20px; padding: 20px; margin-left: 10px;">
+            <span style="color: white; font-size: 20px;">Column 2</span>
+        </div>
+    </div>
+    <div style="background: rgba(255, 255, 255, 0.5); backdrop-filter: blur(5px); border-radius: 10px; padding: 20px; color: #000;">
+        <h2 style="text-align: center;">Main Tab Card</h2>
+        <p>Content goes here</p>
+    </div>
+</div>
+"""
 
-# Get the input field element and register a callback for when the form is submitted
-input_field = form.find("#input-text")
-submit_button = form.find("button")
-submit_button.set_event_handler("click", lambda _: on_submit(input_field.value))
+# Define the HTML for the main tab
+main_tab = """
+<div style="padding: 20px;">
+    <span style="font-size: 20px;">Main Tab Content</span>
+    <br><br>
+    <div style="display: flex; flex-direction: row;">
+        <div style="flex: 1; margin-right: 10px;">""" + card + """</div>
+        <div style="flex: 1;">""" + card + """</div>
+    </div>
+</div>
+"""
 
-# Display the card
-st.markdown("<h1 style='text-align:center'>Glassmorphism Card with Text Input Field</h1>", unsafe_allow_html=True)
-st.components.v1.html(html_result)
+# Define the HTML for the entire page
+page = """
+<div>
+    """ + top_menu + """
+    <div style="display: flex; flex-direction: row;">
+        <div style="flex: 1;">""" + sidebar + """</div>
+        <div style="flex: 4;">""" + main_tab + """</div>
+    </div>
+</div>
+"""
+
+# Render the HTML using the components API
+html_component = html(page)
+st.components.v1.html(html_component)
